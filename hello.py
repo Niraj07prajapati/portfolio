44a98e13fb011e6d081b60b54d253a1708deef3e
Niraj prajapati <prajapatiniraj19@gmail.com>
2024-05-11 18:57:12 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,27 @@
+print("Hello World!")
+a =10
+b = 15
+print(a+b)
+if a>5:
+    print("Niraj is here")
+else:
+    print("You have to save yourself")
+#This is comment 
+x = 60 
+y = "Niraj prajapati"
+print(x,y)
+print(type(x))
+print(type(y))
+x = str(90)
+print(x)
+print(type(x))
+z= float(5)
+print(z)
+#Variable names are case-sensitive.
+'''
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+'''
